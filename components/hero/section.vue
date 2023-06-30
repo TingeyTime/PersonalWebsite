4b0c56cb4bd7e58cbba88bffd69cc1e74f62b94a
Nate Tingey <ntingey@gmail.com>
2023-06-30 09:18:47 -0600

    add hero

--- components/hero/section.vue
+++ components/hero/section.vue
@@ -1,3 +1,7 @@
 <template>
-  <slot></slot>
+  <div class="contain mx-auto bg-[url('/assets/img/bg-masthead.jpg')] bg-auto bg-no-repeat bg-center">
+    <div class="p-[100px]">
+      <slot></slot>
+    </div>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
     <HeroSection>
-
+        <div class="bg-transparent backdrop-blur-3xl mx-[150px] py-[50px] rounded-3xl">
+            <div class="">
+                <img class="rounded-full w-96 h-96 mx-auto" src="@/assets/img/me/bw_circle.png">
+            </div>
+            <div class="text-center p-24">
+                <h1 class="text-4xl font-extrabold tracking-wide sm:text-4xl">
+                    Nathan Tingey
+                </h1>
+                <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+                <h2 class="text-2xl font-bold sm:text-2xl">
+                    <ul>
+                        <li>Information Technology</li>
+                        <li>UX Design</li>
+                        <li>Platform Engineering</li>
+                        <li>Data Analysis</li>
+                    </ul>
+                </h2>
+            </div>
+        </div>
     </HeroSection>
     <BaseSection>
         <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
@@ -17,11 +35,8 @@
             </div>
             <div class="text-center">
                 <h1 class="text-4xl pb-8 font-bold tracking-wide text-white sm:text-6xl">
-                    Nathan Tingey
+                    About Me
                 </h1>
-                <h2 class="text-4xl font-bold text-white sm:text-4xl">
-                    Student Of Information Technology | Tech Enthusiast
-                </h2>
                 <p class="mt-6 text-lg leading-8 text-slate-400">
                     An interest in technology led me to User Experience Design. After starting in and creating the role of Accessibility Specialist at BYU Continuing Education, I realized the power of great design and its impact on our lives. This realization perfectly reflects my lifelong interest in excellent products and the design elements that set them apart from their competition. Soon afterward, I switched my major from Computer Engineering to Information Technology with a personal emphasis on Human-Computer Interactions. This change has allowed me to continue developing a skill set I'd already built and use that skill set to ensure the success of each of the projects I've worked on.
                 </p>
